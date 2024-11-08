63df07af340b86927d3bf2d357787b01fadcbd77
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-08 17:33:16 +0500

    Added tests for the views.

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "posts/index", type: :view do
+  current_user = User.first_or_create!(email: 'jake@ymail.com', password: '123456', password_confirmation: '123456')
   before(:each) do
     assign(:posts, [
       Post.create!(
         title: "Title",
         body: "MyText",
-        user: nil,
-        views: 2
+        user: current_user,
+        views: 14
       ),
       Post.create!(
         title: "Title",
         body: "MyText",
-        user: nil,
-        views: 2
+        user: current_user,
+        views: 12
       )
     ])
   end
@@ -23,7 +24,9 @@ RSpec.describe "posts/index", type: :view do
     cell_selector = 'div>p'
     assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
     assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(current_user.id.to_s), count: 4
+    assert_select cell_selector, text: Regexp.new(14.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new(12.to_s), count: 1
+
   end
 end
