31f77db8d29ab65602b2abcd01916559df51dc44
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-08 15:46:07 +0500

    Added the validations in Scaffold app.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,23 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  include PostsHelper
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post = assign_post_creator(@post, current_user)
 
     respond_to do |format|
       if @post.save
@@ -34,7 +30,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -47,7 +42,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy!
 
@@ -58,13 +52,11 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body, :user_id, :views)
+      params.require(:post).permit(:title, :body)
     end
 end
